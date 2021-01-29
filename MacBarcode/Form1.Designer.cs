2c00c0a2a0fe1395ee6a212f02c9f14c1c2c8023
GT3CH1 <gavinpease@gmail.com>
2021-01-29 08:03:45 -0700

    Make a barcode appear

--- MacBarcode/Form1.Designer.cs
+++ MacBarcode/Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace MacBarcode
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OnBarcode.Barcode;
+
+namespace MacBarcode
 {
     partial class Form1
     {
@@ -32,9 +37,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+            TextBox textBox = new TextBox();
+            PictureBox pbox = new PictureBox();
+            pbox.Width = 500;
+            pbox.Height = 200;
+            
+            textBox.Text = "test";
+            GenerateBacode("test", "C:/Users/gcpease/test.png");
+            Image img = Image.FromFile("C:/Users/gcpease/test.png");
+            pbox.Image = img;
+            pbox.Location = new Point(300, 150);
+            this.Controls.Add(pbox);
         }
 
         #endregion
+        private void GenerateBacode(string _data, string _filename)
+        {
+            BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+            Image img = b.Encode(BarcodeLib.TYPE.CODE39, "5CD8155SX0", Color.Black, Color.White, 290, 120);
+            img.Save(_filename);
+        }
     }
 }
 
--- MacBarcode/Program.cs
+++ MacBarcode/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OnBarcode.Barcode;
 
 namespace MacBarcode
 {
@@ -19,5 +20,6 @@ namespace MacBarcode
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        
     }
 }
